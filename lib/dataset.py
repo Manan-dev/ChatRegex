39a3598bd725e337d44652903050774ecde67aaa
Manan Patel <mananpatel6902@gmail.com>
2023-09-29 15:37:50 -0400

    #6 - Read in data and converted to ascii characters

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,7 +1,32 @@
 """
 Functions for loading and processing text dataset.    
 """
+import os
+import re
 
+"""
+  Removes all non-ascii characters from the text.
+  Parameters:
+    text(string): Text to be converted.
+  Returns:
+    text (string): converted text.
+"""
+def convert_text_to_ascii(text):
+    return text.encode("ascii", "ignore").decode()
 
-def test_dataset_func():
-    print("test_dataset_func: Delete this function.")
+"""
+  Reads the text file and returns a list of lines.
+  Parameters:
+    name_of_file (string): Name of the file to be read.
+  Returns:
+    text (string): Text read from the file.
+"""
+def read_data(name_of_file):
+    file_path = os.path.join("dataset", name_of_file)
+    with open(file_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    
+    text = ''.join(map(str,lines))
+    text = convert_text_to_ascii(text)
+    return text
+
